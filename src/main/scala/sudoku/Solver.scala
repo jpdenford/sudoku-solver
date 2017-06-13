05789764f37a69d429d7456e9e5f1dcee115bceb
jpdenford <jpdenford@gmail.com>
2017-06-13 13:11:05 +1200

    Add small tidy ups

--- src/main/scala/sudoku/Solver.scala
+++ src/main/scala/sudoku/Solver.scala
@@ -1,4 +1,5 @@
 package sudoku
+
 import Solver._
 
 
@@ -7,24 +8,25 @@ object Solver {
   type Square = Set[Int]
   type Board = Vector[Square]
 
-  case class Coord(x: Int, y:Int)
+  case class Coord(x: Int, y: Int)
 
-  case class Area(a: Coord, b: Coord){
+  case class Area(a: Coord, b: Coord) {
     def contains(c: Coord): Boolean = c.x <= b.x && c.x >= a.x && c.y <= b.y && c.y >= a.y
   }
 
   val BOARD_SIZE = 9
 
-  /***
+  /** *
     * Check all rows and colums have values 1 - 9
+    *
     * @param board
     * @return
     */
   def isSolved(board: Board): Boolean = {
     (0 to 8).forall(i => {
       val emptySet = Set[Int]()
-      ((0 to 8).foldLeft(emptySet)((set, j) => (set + board(j*9+i).head)) diff (1 to 9).toSet).isEmpty &&
-      ((0 to 8).foldLeft(emptySet)((set, j) => (set + board(j+i*9).head)) diff (1 to 9).toSet).isEmpty
+      ((0 to 8).foldLeft(emptySet)((set, j) => (set + board(j * 9 + i).head)) diff (1 to 9).toSet).isEmpty &&
+        ((0 to 8).foldLeft(emptySet)((set, j) => (set + board(j + i * 9).head)) diff (1 to 9).toSet).isEmpty
     })
   }
 
@@ -39,26 +41,27 @@ object Solver {
     }
   }
 
-  def hasValue(sq: Square):Boolean = sq.size == 1
+  def hasValue(sq: Square): Boolean = sq.size == 1
 
   def firstUnknownIndex(board: Board): Option[Int] = board.zipWithIndex.find(x => !hasValue(x._1)).flatMap(x => Some(x._2))
 
   /**
-    * Set the value at the given index
-    * corresponding row, col and quadrant will have value removed
-    * Will return
+    * Set the value at the given index.
+    * Corresponding row, col and quadrant squares will have value removed
+    *
     * @param index the index on the board
     * @param value the value to set the given index to
     * @param board the board
     * @return
     */
   def set(index: Int, value: Int, board: Board): Option[Board] = {
-    val newBoard = mapBoard(_ - value, connectedSquares(index), board)
-    newBoard.map(b => b.updated(index, Set(value))) // set the actual square
+    mapBoard(_ - value, connectedSquares(index), board)
+      .map(b => b.updated(index, Set(value))) // set the actual square
   }
 
-  /***
+  /** *
     * Get a coordinate of a square given the index
+    *
     * @param index
     * @return
     */
@@ -72,9 +75,10 @@ object Solver {
     Area(Coord(minX, minY), Coord(minX + quadSize - 1, minY + quadSize - 1))
   }
 
-  /***
+  /** *
     * Returns a predicate which tests if a given index i is in
     * the same row, column or quadrant as n (excluding actual index)
+    *
     * @param n
     * @return predicate function
     */
@@ -91,9 +95,10 @@ object Solver {
     (i: Int) => (i != n) && (inColumn(i) || inRow(i) || inQuad(i))
   }
 
-  /***
+  /** *
     * Map an operation over a board for the valid squares
     * Option will contain board if operation is valid for all mapped squares
+    *
     * @param op
     * @param pred predicate for applying op to square
     * @param board
@@ -101,23 +106,25 @@ object Solver {
     */
   def mapBoard(op: Square => Square, pred: Int => Boolean, board: Board): Option[Board] = {
     def map(board: Board, index: Int): Option[Board] = {
-      if(index >= board.size) return Some(board)
-      if(!pred(index)) return map(board, index + 1)
+      if (index >= board.size) return Some(board)
+      if (!pred(index)) return map(board, index + 1)
       val newBoard = board.updated(index, op(board(index)))
-      if(newBoard(index).isEmpty) return None // operation was invalid as square now empty
-      else if(newBoard(index).size == 1 && board(index).size != 1){ //operation made this square single value
+      if (newBoard(index).isEmpty) return None // operation was invalid as square now empty
+      else if (newBoard(index).size == 1 && board(index).size != 1) {
+        //operation made this square single value
         val boardUpdated = set(index, newBoard(index).head, newBoard)
-        if(boardUpdated.isEmpty) return None // couldn't continue with update
+        if (boardUpdated.isEmpty) return None // couldn't continue with update
         else return map(boardUpdated.get, index + 1) // successful, continue
       }
       return map(newBoard, index + 1)
     }
+
     map(board, 0)
   }
 
   def stringify(squares: Board): String = {
     squares.grouped(BOARD_SIZE).map(_.map(x => {
-      val vals = if(x.size == 1) x.head.toString else ""
+      val vals = if (x.size == 1) x.head.toString else ""
       val padding = List.fill(2 - vals.length)(" ").mkString
       s"| ${vals + padding}"
     }).mkString).mkString("\n")
@@ -130,11 +137,11 @@ class UnsolvableException extends RuntimeException
 object Do extends App {
   val board = loadSudoku(3)
   val start = System.currentTimeMillis()
-  println("starting:\n"+stringify(board))
+  println("starting:\n" + stringify(board))
   val solvedBoard = solve(board).get
   val end = System.currentTimeMillis()
-  println("---------------SOLVED-----------------")
+  println("-------------SOLVED---------------")
   println(stringify(solvedBoard))
-  println("Took: " + (end - start) + " millis")
+  println("Took: " + (end - start) + " ms")
 }
 
--- src/test/scala/sudoku/SudokuSuite.scala
+++ src/test/scala/sudoku/SudokuSuite.scala
@@ -12,7 +12,6 @@ class SudokuSuite extends FunSuite  {
   lazy val boards = loadSudoku
   val emptyBoard = boards.head
   val b1 = boards(1)
-//  val b2 = boards(2)
 
   test("getter") {
     assert(b1(0) === Set(2))
@@ -35,45 +34,6 @@ class SudokuSuite extends FunSuite  {
     assert((0 to 80).filter(connectedSquares(39)).sorted === Vector(3, 12, 21, 30, 31, 32, 36, 37, 38, 39, 40, 41, 42, 43, 44, 48, 49, 50, 57, 66, 75))
   }
 
-
-//  test("updating board2") {
-//    val nb = set(5, getIndex(Coord(5,7)), b1)
-//    assert(nb.get(Coord(5,7)) === Set(5))
-//  }
-
-  /* test("setting value updates row"){
-     val nb = emptyBoard.set(1, Coord(0,0))
-     println(nb)
-     assert((1 to 8).map(n => nb.get(Coord(n, 0))) === Vector.fill(8)(Set(2,3,4,5,6,7,8,9)))
-   }
-
-   test("setting value updates col"){
-     val nb = emptyBoard.set(1, Coord(0,0))
-     assert((1 to 8).map(n => nb.get(Coord(0, n))) === Vector.fill(8)(Set(2,3,4,5,6,7,8,9)))
-   }
-
-   test("setting value updates quad"){
-     //setting value to 1 should remove it from all possibilities in the quad
-     val nb = emptyBoard.set(1, Coord(0,0))
-     val range = (0 to 2).toList
-     val actual:List[Square] = for {
-         y <- range
-         x <- range
-       } yield nb.get(Coord(x, y))
-     assert(actual.toList === (Set(1)::List.fill(8)(Set(2,3,4,5,6,7,8,9))) )
-   }
-
-   test("first unknown"){
-     assert(emptyBoard.firstUnknown() === Coord(0, 0))
-   }*/
-
-//  test("index to coord and coord to index") {
-//    assert(getCoord())
-//  }
-
-//  test("first unknown2"){
-//    assert(b2.firstUnknown() === Coord(5, 1))
-//  }
 }
 
 
